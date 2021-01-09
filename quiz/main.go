8f42ed3aa259c3e3e31ced989423c47e84b64088
Har Har Mahadev! <0xmahadev@gmail.com>
2021-01-09 12:27:18 +0545

    [edit] refactored the csv parser in quiz

--- quiz/main.go
+++ quiz/main.go
@@ -4,20 +4,35 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"strings"
 	"time"
 )
 
-type Question struct {
+type question struct {
 	Text   string
 	Answer string
 }
 
-var questions = make([]Question, 0)
+func parseCSV(fileName string) *[]question {
+
+	f, _ := os.Open(fileName)
+	rdr := csv.NewReader(f)
+
+	allLines, _ := rdr.ReadAll()
+
+	questions := make([]question, len(allLines))
+
+	for i, v := range allLines {
+		questions[i] = question{Text: v[0], Answer: strings.TrimSpace(v[1])}
+	}
+
+	return &questions
+
+}
 
 func main() {
+
 	var (
 		fileName = flag.String("file", "problems.csv", "CSV File to Read Problems From")
 		duration = flag.Int64("time", 5, "Time Duration To Wait for Answer")
@@ -25,36 +40,24 @@ func main() {
 
 	flag.Parse()
 
-	f, err := os.Open(*fileName)
-
-	if err != nil {
-		fmt.Println("Unable to open file")
-		return
-	}
-
-	csvReader := csv.NewReader(f)
-
-	for {
-
-		rawText, err := csvReader.Read()
-
-		if err == io.EOF {
-			break
-		}
-		questions = append(questions, Question{Text: rawText[0], Answer: strings.TrimSpace(rawText[1])})
-	}
 	userAnswer := make(chan string)
 	var answer string
 	var correctAnswer int
-	timer := time.NewTimer(time.Duration(*duration) * time.Second)
-	for i, qn := range questions {
+	questions := parseCSV(*fileName)
 
+	// this makes a timer which sends me a message in timer.C, after the time
+	// is passed.
+	timer := time.NewTimer(time.Duration(*duration) * time.Second)
+
+	for i, qn := range *questions {
 		fmt.Printf("Question #%d: %s", i+1, qn.Text)
+
 		go func() {
 			fmt.Printf("\nYour Answer: ")
 			fmt.Scanf("%s", &answer)
 			userAnswer <- answer
 		}()
+
 		select {
 
 		case <-timer.C:
@@ -68,5 +71,5 @@ func main() {
 		}
 
 	}
-	fmt.Printf("You answered %d/%d questions correctly\n", correctAnswer, len(questions))
+	fmt.Printf("You answered %d/%d questions correctly\n", correctAnswer, len(*questions))
 }
